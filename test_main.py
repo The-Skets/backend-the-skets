788e9e7a9c2c240dda093954fc57177d8bdd6ca2
carter-0 <carterannandale@gmail.com>
2022-10-19 11:10:09 +0000

    Add test for sign_in

--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
-
+from flask import session
 from main import app
+from config import env
 
 
 class Test(TestCase):
@@ -20,7 +21,17 @@ class Test(TestCase):
         assert response.json[0]["quality"] == "480p"
 
     def test_v1_private_sign_in(self):
-        response = self.client.post("/v1/get_performances", data=())
+        with self.client:
+            response = self.client.post("/v1/private/sign_in", json={
+                "username": env["TEST_USERNAME"],
+                "password": env["TEST_PASSWORD"]
+            })
+
+            assert response.status_code == 200
+            assert response.json["status"] == "success"
+            assert session["logged_in"] is True
+            assert session["profile"]["name"] == "carter"
+
 
 if __name__ == "__main__":
     main()
